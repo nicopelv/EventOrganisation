d5d49a375f3144048c9ec0f304e628a7aa2bb093
nicopelv <pelvet_nicolas@hotmail.fr>
2016-01-19 10:00:39 +0000

    redirect contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,9 @@ class ContactsController < ApplicationController
   def send_message
 
    UserMailer.welcome_email(params[:contacts]).deliver_now
+   
+   redirect_to(root_path)
+   
   end
   
 end
